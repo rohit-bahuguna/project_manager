04ebc6b9e952b5b4789869c75f71f4c7f061bc22
rohit-bahuguna <rohitbahuguna.4446@gmail.com>
2023-04-13 15:59:41 +0530

    cookies option changed

--- utils/CookieToken.js
+++ utils/CookieToken.js
@@ -2,7 +2,7 @@ const cookieToken = (res, user, workspace = '', message) => {
 	const token = user.getJwtToken(process.env.JWT_EXPIRY);
 	const cookieOptions = {
 		sameSite: 'none',
-		secure: true,
+		secure: false,
 		httpOnly: true,
 		expires: new Date(
 			Date.now() + process.env.COOKIE_TIME * 24 * 60 * 60 * 1000
